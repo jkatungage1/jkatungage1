8d02f256c9f738539127690b55be12c5aabdf333
Jacques Katunga Mukendi (Nokia) <jacques.katunga_mukendi@nokia.com>
2025-03-19 18:07:30 +0100

    this commiting process has been completely automated

--- Repo_manager.py
+++ Repo_manager.py
@@ -1,4 +1,4 @@
-import sys, os, subprocess, time, git; from pathlib import Path
+import sys, os, subprocess, time, git, shutil; from pathlib import Path
 from git import Repo
 PROJECT_DIR = os.path.normcase(Path(__file__).parent)
 
@@ -150,6 +150,60 @@ def git_commit(*args):
 
 
         except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error: {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else "Uknonwn error")
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            exit(0)
+
+def git_clone(*args) -> str:
+    # print('Running git pull...')
+    if "S" in args:
+        print("Using Subprocess module")
+        # USING SUBPROCESS MODULE
+        try :
+            repo_url = input("Enter repository url: ")
+            repo_name = repo_url.split('/')[-1].split('.')[0]
+            destination_folder = input("Enter destination folder(absolute path): ")
+            destination_folder = os.path.normcase(destination_folder)
+            command = subprocess.run(["git", "clone", f'{repo_url}'], cwd=destination_folder,check=True)
+            assert command.returncode == 0
+            print('Git clone complete.')
+            input("Press Enter to continue...")
+
+            # command = os.system(f'RMDIR /S "{destination_folder}\\{repo_name}"')
+
+            # time.sleep(5)
+
+        except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else "Uknonwn error")
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            # exit(0)
+
+    if "O" in args:
+        print("Using OS module")
+        # USING OS MODULE
+        try :
+            command = os.system(f"cd {PROJECT_DIR} && git add -u")
+            commit_message = input("Enter commit message: ")
+            command = os.system(f'cd {PROJECT_DIR} && git commit -m "{commit_message}"')
+            assert command == 0
+            print('Git status complete.')
+            # time.sleep(5)
+
+        except AssertionError as e:
             print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
             exit(1)
 
@@ -161,6 +215,44 @@ def git_commit(*args):
             print("Finally block executed.")
             exit(0)
 
+    else:
+        # USING git MODULE
+        print("Using git module")
+        try :
+            deployment_repo = Repo(PROJECT_DIR)
+            assert not deployment_repo.bare
+            master = deployment_repo.heads.master
+            # git_pull()
+            log = master.log()
+            print(log[0])
+
+            git_cmd = deployment_repo.git
+            # if not git_cmd.checkout("master"):
+            #     None# git_cmd.checkout("master",b="Test")
+            if "nothing" in git_cmd.status() :
+                print("No changes to commit")
+                exit(0)
+            else :
+                commit_message = input("Enter commit message: ")
+                assert git_cmd.add("-u") == ''
+                assert git_cmd.commit("-m", f"{commit_message}") != ''
+
+
+        except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else "Uknonwn error")
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            exit(0)
+            
+    return repo_name
+
+
 def main(*args:str):
     os.system('cls' if os.name == 'nt' else 'clear')
     print(fr"{PROJECT_DIR}")
@@ -168,17 +260,24 @@ def main(*args:str):
     for car in args:
         args[args.index(car)] = car.upper()
 
+    os.system(fr'echo "Bonjour" > "{PROJECT_DIR}\deplo\{git_clone(*args)}\bonjour.txt"')
+    input()
     git_commit(*args)
-    git_pull(*args)
+    # git_pull(*args)
 
 
 if __name__ == '__main__':
-
+    os.system(f'cd {PROJECT_DIR}')
     if len(sys.argv) > 1:
         for i, arg in enumerate(sys.argv[1:], start=1):
             print(f"Argument {i}: {arg}")
             time.sleep(2)
     else: print("No arguments provided.")
-    main(sys.argv[1] if len(sys.argv) > 1
-         else sys.argv[1],sys.argv[2] if len(sys.argv) > 2 else "")
+    try :
+        main("s")
+        # main(sys.argv[1] if len(sys.argv) > 1
+        #     else sys.argv[1],sys.argv[2] if len(sys.argv) > 2 else "")
+    except Exception as e:
+        print(f"Error: {e}")
+
 
