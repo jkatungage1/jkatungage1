07e99e33f8922e768e39690013392910ec6b6d2a
Jacques Katunga Mukendi (Nokia) <jacques.katunga_mukendi@nokia.com>
2025-03-19 14:33:55 +0100

    Second commit from subprocess module

--- Repo_manager.py
+++ Repo_manager.py
@@ -1,14 +1,174 @@
-import os, time; from pathlib import Path; from git import Repo
-PROJECT_DIR = Path(__file__).parent
+import os, subprocess, time, git; from pathlib import Path
+from git import Repo
+PROJECT_DIR = os.path.normcase(Path(__file__).parent)
 
+def git_pull(*args) :
+    # print('Running git pull...')
+    if "s" in args:
+        print("Using Subprocess module")
+        # USING SUBPROCESS MODULE
+        try :
+            command = subprocess.run(["git", "status"], cwd=PROJECT_DIR, check=True)
+            print('Git status complete.')
+            assert True
+            # time.sleep(5)
 
-def main(*args):
+        except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else 1)
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            exit(0)
+
+    if "o" in args:
+        print("Using OS module")
+        # USING OS MODULE
+        try :
+            command = os.system(f"cd {PROJECT_DIR} && git status")
+            assert True
+            print('Git status complete.')
+            # time.sleep(5)
+
+        except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else 1)
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            exit(0)
+
+    else:
+        # USING git MODULE
+        print("Using git module")
+        try :
+            deployment_repo = Repo(PROJECT_DIR)
+            assert not deployment_repo.bare
+            master = deployment_repo.heads.master
+            log = master.log()
+            print(log[0])
+
+            git_cmd = deployment_repo.git
+            if not git_cmd.checkout("master"):
+                None# git_cmd.checkout("master",b="Test")
+
+            assert git_cmd.pull("origin","master")
+            # time.sleep(5)
+
+        except git.exc.GitCommandError as e:
+            print(f"Git Command Error: {e}")
+
+        except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else 1)
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            exit(0)
+
+def git_commit(*args):
+    # print('Running git pull...')
+    if "s" in args:
+        print("Using Subprocess module")
+        # USING SUBPROCESS MODULE
+        try :
+            command = subprocess.run(["git", "add" , "-u"], cwd=PROJECT_DIR, check=True)
+            commit_message = input("Enter commit message: ")
+            command = subprocess.run(["git", "commit", "-m", commit_message], cwd=PROJECT_DIR, check=True)
+            assert command.returncode == 0
+            print('Git commit complete.')
+            # time.sleep(5)
+
+        except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else 1)
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            exit(0)
+
+    if "o" in args:
+        print("Using OS module")
+        # USING OS MODULE
+        try :
+            command = os.system(f"cd {PROJECT_DIR} && git add -u")
+            commit_message = input("Enter commit message: ")
+            command = os.system(f"cd {PROJECT_DIR} && git commit -m {commit_message}")
+            assert command == 0
+            print('Git status complete.')
+            # time.sleep(5)
+
+        except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else 1)
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            exit(0)
+
+    else:
+        # USING git MODULE
+        print("Using git module")
+        try :
+            deployment_repo = Repo(PROJECT_DIR)
+            assert not deployment_repo.bare
+            master = deployment_repo.heads.master
+            log = master.log()
+            print(log[0])
+
+            git_cmd = deployment_repo.git
+            if not git_cmd.checkout("master"):
+                None# git_cmd.checkout("master",b="Test")
+
+            assert git_cmd.pull("origin","master")
+            # time.sleep(5)
+
+        except git.exc.GitCommandError as e:
+            print(f"Git Command Error: {e}")
+
+        except AssertionError as e:
+            print("Unknown Assertion Error" if str(e) == "" else f"Assertion Error : {e}")
+            exit(1)
+
+        except Exception as e:
+            print(f"Error: {e}" if "git" in str(e) else 1)
+            exit(1)
+
+        finally:
+            print("Finally block executed.")
+            exit(0)
+
+def main(*args:str):
     os.system('cls' if os.name == 'nt' else 'clear')
-    print('Running git pull...')
+    print(fr"{PROJECT_DIR}")
+    for car in args:
+        car.upper()
 
-    deployment_repo = Repo(PROJECT_DIR)
-    deployment_repo.git.pull()
+    # git_pull(*args)
+    git_commit(*args)
 
-    print('Git pull complete.')
-    time.sleep(5)
-    exit("Finished")
+
+
+
+if __name__ == '__main__':
+    main("s")
